49dce8e4d4c5c19ede1c649319efa2d885840775
dima <cinnamonbun1233@yandex.ru>
2023-07-01 12:20:51 +0300

    Добавил файлы конфигурации

--- account-service/src/main/java/com/javastart/account/AccountApplication.java
+++ account-service/src/main/java/com/javastart/account/AccountApplication.java
@@ -1,5 +1,7 @@
 package com.javastart.account;
 
 public class AccountApplication {
+    public static void main(String[] args) {
 
+    }
 }
--- bill-service/src/main/java/com/javastart/bill/BillApplication.java
+++ bill-service/src/main/java/com/javastart/bill/BillApplication.java
@@ -1,5 +1,7 @@
 package com.javastart.bill;
 
 public class BillApplication {
+    public static void main(String[] args) {
 
+    }
 }
